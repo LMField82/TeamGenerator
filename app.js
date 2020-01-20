08a4884ac24ed13643491aad3eaf36704a736268
LindsayMF <meneslindsay@hotmail.com>
2020-01-19 18:41:16 -0700

    added prompts and get employee function to app.js file

--- app.js
+++ app.js
@@ -3,6 +3,40 @@ const inquirer = require("inquirer");
 const htmlRender = require("./library/htmlRender")
 
 
+
+
+const Manager = () => {
+    inquirer.prompt({
+        message: "What is your office number?",
+        type: "input",
+        name: "officeNumber"
+    }).then(({officeNumber}) => {
+        const officeNum = this.officeNumber;
+        
+    });
+}
+
+const Engineer = () => {
+    inquirer.prompt({
+        message: "What is your GitHub user name?",
+        type: "input",
+        name: "githubUserName"
+    }).then(({githubUserName}) => {
+        const githubUser = this.githubUserName;
+    });
+}
+
+const Intern = () => {
+    inquirer.prompt({
+        message: "What is the name of your school?",
+        type: "input", 
+        name: "school"
+    }).then(({school}) => {
+        const schoolName = this.school;
+    });
+}
+
+const getEmployeeInfo = () => {
 inquirer.prompt([
     {
         message: "What is your name?",
@@ -24,18 +58,25 @@ inquirer.prompt([
         type: "list",
         name: "title",
         choices: ["Manager", "Engineer", "Intern"]
+    },
+
+]).then(({title}) => {
+    switch(title) {
+        case "Manager":
+            Manager();
+            break;
+        case "Engineer":
+            Engineer();
+            break;
+        case "Intern":
+            Intern();
+            break;
     }
-]).then(({name, id, email, title}) => {
-    console.log("-----------------------");
-    console.log("name", name);
-    console.log("id", id);
-    console.log("email", email);
-    console.log("title", title);
-    console.log("-----------------------");  
-    getName();
-    getId();
-    getEmail();
-    getRole();
 
+   
 
-})
+});
+
+}
+getEmployeeInfo();
+
